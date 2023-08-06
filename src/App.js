64fe918b021b35fde6fd487d038490cd63536462
Symbiotic Love <matthew@symbiotic.love>
2023-08-06 09:18:52 -0700

    Upload demo

---
+++ src/App.js
@@ -0,0 +1,220 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import StoryHistory from './components/StoryHistory';
+
+function App() {
+  const [prompt, setPrompt] = useState('');
+  const [author, setAuthor] = useState('');
+  const [genre, setGenre] = useState('');
+  const [ageGroup, setAgeGroup] = useState('');
+  const [output, setOutput] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [storyHistory, setStoryHistory] = useState([]);
+  const [customAuthor, setCustomAuthor] = useState('');
+  const [customGenre, setCustomGenre] = useState('');
+
+  const authors = [
+    "J.K. Rowling", 
+    "William Shakespeare", 
+    "Stephen King", 
+    "Jane Austen", 
+    "Mark Twain", 
+    "George Orwell", 
+    "Agatha Christie", 
+    "J.R.R. Tolkien", 
+    "Isaac Asimov", 
+    "Maya Angelou"
+  ];
+  
+  const genres = [
+    "Horror", 
+    "Comedy", 
+    "Adventure", 
+    "Romance", 
+    "Mystery", 
+    "Fantasy", 
+    "Science Fiction", 
+    "Historical", 
+    "Thriller", 
+    "Drama"
+  ];
+  
+  const ageGroups = [
+    "Board Books (0-3)", 
+    "Picture Books (3-8)", 
+    "Middle Grade (8-12)", 
+    "Young Adult (12-18)", 
+    "New Adult (18-30)", 
+    "Adult (30+)"
+  ];  
+
+  const handlePromptChange = (event) => {
+    setPrompt(event.target.value);
+  };
+
+  const handleAuthorChange = (event) => {
+    setAuthor(event.target.value);
+  };
+
+  const handleGenreChange = (event) => {
+    setGenre(event.target.value);
+  };
+
+  const handleAgeGroupChange = (event) => {
+    setAgeGroup(event.target.value);
+  };
+
+  const handleCustomAuthorChange = (event) => {
+    setCustomAuthor(event.target.value);
+  };
+  
+  const handleCustomGenreChange = (event) => {
+    setCustomGenre(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const chosenAuthor = customAuthor || author;
+    const chosenGenre = customGenre || genre;
+
+    const messageContent = `Write a ${chosenGenre} story. ${prompt} in the style of ${chosenAuthor} for the age group ${ageGroup}.`;
+
+    try {
+      const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'user',
+                content: messageContent
+            }
+        ],
+        max_tokens: 100,
+      }, {
+        headers: {
+          'Authorization': `Bearer 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`,
+          'Content-Type': 'application/json'
+        }
+      });
+
+      setOutput(response.data.choices[0].message.content.split('\n\n'));
+      
+      const storyContent = response.data.choices[0].message.content.split('\n\n').join(' ');  // Converting paragraphs to a single string for the history list
+      setStoryHistory([storyContent, ...storyHistory]);
+
+      const storyDetails = {
+        content: response.data.choices[0].message.content.split('\n\n').join(' '),
+        prompt,
+        author,
+        genre,
+        ageGroup
+      };
+  
+      setStoryHistory([storyDetails, ...storyHistory]);
+
+    } catch (err) {
+        console.error(err);
+    } finally {
+        setLoading(false);
+    }
+};
+
+const continueStory = async (lastParagraph, author, genre, ageGroup) => {
+  setLoading(true);
+
+  const continuationPrompt = `Continue this ${genre} story. ${lastParagraph} in the style of ${author} for the age group ${ageGroup}.`;
+
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: continuationPrompt
+        }
+      ],
+      max_tokens: 100,
+    }, {
+      headers: {
+        'Authorization': `Bearer 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`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const newParagraphs = response.data.choices[0].message.content.split('\n\n');
+    const updatedStory = [...output, ...newParagraphs].join('\n\n');
+
+    setOutput(updatedStory.split('\n\n'));
+
+    // Find the index of the last story in the history that you want to continue
+    const lastIndex = storyHistory.findIndex(story => story.content === lastParagraph);
+
+    if (lastIndex !== -1) {
+      const updatedStoryHistory = [...storyHistory];
+      updatedStoryHistory[lastIndex].content = updatedStory;
+      setStoryHistory(updatedStoryHistory);
+    }
+
+  } catch (err) {
+    console.error(err);
+  } finally {
+    setLoading(false);
+  }
+};
+
+  return (
+    <div className="App">
+      <div className="app-content">
+        <StoryHistory 
+            history={storyHistory} 
+            continueStory={continueStory}
+            currentParams={{ author, genre, ageGroup }}
+          />
+        <form onSubmit={handleSubmit}>
+          <h1>What is Your Story About?</h1>
+          <select value={genre} onChange={handleGenreChange}>
+            <option value="">Give me a __ story.</option>
+            {genres.map((genre, index) => <option key={index} value={genre}>{genre}</option>)}
+          </select>
+          <input 
+            type="text" 
+            value={customGenre} 
+            onChange={handleCustomGenreChange} 
+            placeholder="Or your own genre..."
+          />
+          <textarea value={prompt} onChange={handlePromptChange} placeholder="(shorter is better!) it's a story about..."/>
+          <select value={author} onChange={handleAuthorChange}>
+            <option value="">In the style of...</option>
+            {authors.map((author, index) => <option key={index} value={author}>{author}</option>)}
+          </select>
+          <input 
+            type="text" 
+            value={customAuthor} 
+            onChange={handleCustomAuthorChange} 
+            placeholder="Don't see your favorite author?"
+          />
+          <select value={ageGroup} onChange={handleAgeGroupChange}>
+            <option value="">For the age group...</option>
+            {ageGroups.map((ageGroup, index) => <option key={index} value={ageGroup}>{ageGroup}</option>)}
+          </select>
+          <div className="button-contain">
+            <button type="submit" className="create-button">Create</button>
+          </div>
+        </form>
+      </div>
+      <div className="story-div">
+        <h2>Your Story:</h2>
+        {loading ? <p>Loading...</p> : (
+          <>
+            {output.map((paragraph, index) => (
+              <p key={index}>{paragraph}</p>
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StoryHistory.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import './StoryHistory.css';
+
+function StoryHistory({ history, continueStory, currentParams }) {
+  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentStory, setCurrentStory] = useState('');
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  const getTitle = (storyDetails) => {
+    const firstSentence = storyDetails.content.split(/[.!?]/)[0];
+    
+    if (firstSentence.length <= 50) return firstSentence + '.';
+    
+    return firstSentence.substring(0, 47) + '...';
+  };
+
+  const handleStoryClick = (event, content) => {
+    event.stopPropagation();
+    if (isModalOpen) {
+      closeModal();
+      return;
+    }
+    setCurrentStory(content);
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setIsModalOpen(true);
+    }, 10);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setIsTransitioning(true);
+  };
+
+  useEffect(() => {
+    if (isModalOpen) {
+      setIsTransitioning(false);
+    }
+  }, [isModalOpen]);
+
+  const downloadStoryAsTextFile = (content) => {
+    // Generate filename using the first 7 characters of the story
+    const filename = content.substring(0, 7) + '....txt';
+
+    // Create a blob from the content
+    const blob = new Blob([content], { type: 'text/plain;charset=utf-8' });
+    const element = document.createElement('a');
+    element.href = URL.createObjectURL(blob);
+    element.download = filename;
+    document.body.appendChild(element); 
+    element.click(); 
+    document.body.removeChild(element);
+  };
+
+  return (
+    <div className="story-history">
+      <h2>Story History</h2>
+      <ul>
+        {history.map((storyDetails, index) => (
+          <li key={index}>
+            <span onClick={(event) => handleStoryClick(event, storyDetails.content)}>
+              {getTitle(storyDetails)}
+            </span>
+            <details>
+              <summary>Details</summary>
+              <p><strong>Prompt:</strong> {storyDetails.prompt}</p>
+              <p><strong>Author Style:</strong> {storyDetails.author}</p>
+              <p><strong>Genre:</strong> {storyDetails.genre}</p>
+              <p><strong>Age Group:</strong> {storyDetails.ageGroup}</p>
+            </details>
+          </li>
+        ))}
+      </ul>
+
+      {(isModalOpen || isTransitioning) && (
+        <div className={`modal-background ${isModalOpen ? 'show' : ''}`} onClick={closeModal}>
+          <div className="modal-content" onClick={e => e.stopPropagation()}>
+            {currentStory}
+            <button 
+                onClick={() => downloadStoryAsTextFile(currentStory)}
+                className='dl-button'
+                >
+                Download Story
+            </button>
+            <button 
+              onClick={() => {
+                // Get the last 50 characters of the current story
+                const last50Chars = currentStory;
+                
+                continueStory(last50Chars, currentParams.author, currentParams.genre, currentParams.ageGroup);
+                closeModal();  // Close the modal after continuing the story
+              }}
+              className='continue-button'
+            >
+              Continue Story
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default StoryHistory;
